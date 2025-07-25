c6dc49546f9bfe2e274407960a7d24cbe8c2d405
Matheus <theuszero@gmail.com>
2025-07-25 06:49:33 -0300

    teste de resetar senha separado em outro arquivo de teste

--- test/login.test.js
+++ test/login.test.js
@@ -62,26 +62,3 @@ describe('Login', () => {
         })    
     })
 })
-
-describe('POST /resetar-senha', () => {
-  it('Deve retornar 200 e a mensagem "Senha resetada com sucesso"', async function () {
-    const res = await request(app)
-      .post('/resetar-senha')
-      .send({ usuario: 'Matheus', novaSenha: 'novaSenha123' });
-    expect(res.status).to.equal(200);
-    expect(res.body.message).to.match(/resetada/);
-    const user = usuarios.find(u => u.usuario === 'Matheus');
-    expect(user.senha).to.equal('novaSenha123');
-    expect(user.tentativas).to.equal(0);
-    expect(user.bloqueado).to.be.false;
-    
-  });
-
-  it('Deve retornar 401 e a mensagem "Credenciais inválidas" quando eu tentar resetar a senha de um usuário inexistente', async function () {
-    const res = await request(app)
-      .post('/resetar-senha')
-      .send({ usuario: 'Yuri', novaSenha: 'qualquer' });
-    expect(res.status).to.equal(401);
-    expect(res.body.message).to.match(/credenciais/i);
-  });
-});
